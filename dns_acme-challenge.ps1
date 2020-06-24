be8e29795d1af3927b5bc91e54d77b5980cef7fb
Teremshonok Anton <TerAnYu@yandex.com>
2020-06-24 20:45:09 +0700

    Небольшие изменения; - редактирование таймаута; - исправление проблемы "Internet Explorer".

--- dns_acme-challenge.ps1
+++ dns_acme-challenge.ps1
@@ -1,3 +1,11 @@
+<#
+"Yandex.DNS letsencrypt"
+Author	: TerAnYu
+GitHub	: https://github.com/TerAnYu/yaconnect_dns_acme-challenge
+Date	: 24/06/2020
+Version	: 0.2
+#>
+
 Param(
  [string]$action,
  [string]$domain,
@@ -6,21 +14,26 @@ Param(
 )
 
 $subdomain = $subdomain -replace "([_a-zA-Z0-9].+).($domain)",'$1'
+$timeoutsec = 600
+
+# https://yandex.ru/dev/pdd/doc/concepts/access-docpage/
 $token="YOUR_TOKEN_PDD______________________________________"
 
-
+# https://github.com/PKISharp/win-acme/wiki/DNS-validation-plugins
 
 if ( $action -eq "create" ) {
 iwr https://pddimp.yandex.ru/api2/admin/dns/add `
+	-UseBasicParsing `
     -Method 'POST' `
     -Headers @{'PddToken' = $token} `
-    -Body @{'domain'=$domain;'type'='TXT';'subdomain'=$subdomain;'ttl'='10';'content'=$text}
+    -Body @{'domain'=$domain;'type'='TXT';'subdomain'=$subdomain;'ttl'='5';'content'=$text}
+Wait-Event -Timeout $timeoutsec
 }
-
 
 elseif ( $action -eq "delete" ) {
 $records=(
 iwr https://pddimp.yandex.ru/api2/admin/dns/list `
+    -UseBasicParsing `
     -Headers @{'PddToken' = $token} `
     -Body @{'domain'=$domain}
 ).Content
@@ -32,10 +45,12 @@ $record_id=(($xml).SelectNodes("//Property") | Where {$_.'#text' -eq '_acme-chal
 
 if ($record_id -ne "") {
 iwr https://pddimp.yandex.ru/api2/admin/dns/del `
+    -UseBasicParsing `
     -Method 'POST' `
     -Headers @{'PddToken' = $token} `
     -Body @{'domain'=$domain;'type'='TXT';'record_id'=$record_id}
 }
+Wait-Event -Timeout $timeoutsec
 }
 
 else { Write-Warning("Error action!") }
